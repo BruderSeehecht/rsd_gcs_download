a8d6cae3b999c4386ee70b66110a1aa24ba3ec0a
Marcel <156421464+BruderSeehecht@users.noreply.github.com>
2024-11-13 13:49:21 +0100

    Update rsd_data_query.sql

--- rsd_data_query.sql
+++ rsd_data_query.sql
@@ -1,4 +1,9 @@
 # sql script to query the bigquery public dataset for landsat and sentinel 2 data and to create a table which can be used to bulk download the data from gcs
+# choose the desired sensor by commenting out the other
+# sentinel-2 l1c data is accessible by using "sentinel_2_index" as the table_name
+# additional filtering is possible by extracting more values from different collums 
+
+  
 # landsat data access
 SELECT 
 *
@@ -9,7 +14,7 @@ WHERE
   AND spacecraft_id = "LANDSAT_7"
   AND wrs_row = 69 OR wrs_row = 75
 
-# sentinel 2 data access 
+# sentinel 2 l2a data access 
 SELECT
 *
 FROM `bigquery-public-data.cloud_storage_geo_index.sentinel_2_sr_index`
